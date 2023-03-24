0657477e13ab42a649398e90b405315cdd5cef37
Quincy Qiu <qiuqfang@outlook.com>
2023-03-24 11:23:18 +0800

    chore: 添加 auto import 插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 import Icons from "unplugin-icons/vite";
+import AutoImport from "unplugin-auto-import/vite";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     Icons({
       compiler: "svelte",
+    }),
+    AutoImport({
+      imports: ["svelte", { "$app/stores": ["getStores", "navigating", "page", "updated"] }],
+      dts: true,
+      eslintrc: {
+        enabled: true, // <-- this
+      },
     }),
   ],
   test: {
